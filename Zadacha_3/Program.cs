92a6a66cd21b7686a7f5f05a5abec9099f07d2af
Oleynik Andrey <andry44666@gmail.com>
2023-02-19 19:05:47 +0300

    Comlited Zadacha_3

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 44:выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5
+// --> 0 1 1 2 3
+// Если N = 3
+// --> 0 1 1
+// Если N = 7
+// --> 0 1 1 2 3 5 8
+
+System.Console.WriteLine("Введите число N: ");
+PrintFibonacci(int.Parse(Console.ReadLine()));
+
+int Fibonacci (int n)  // Метод для вычисления числа Фибоначчи
+{
+return n > 1 ? Fibonacci(n - 1) + Fibonacci(n - 2) : n;
+}
+
+int PrintFibonacci (int n)  //  Вывод последовательности Фибоначчи
+{
+
+  for (int i = 0; i < n; i++)
+  {
+    System.Console.WriteLine(Fibonacci(i));
+  }
+  return Fibonacci(n);
+}
